57f8420efb5c04b4d0c7ff52dfb80a5c78854fe0
Aicha-ch <chwikhi.aycha@gmail.com>
2024-12-05 17:23:16 +0100

    add documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+This module defines a function matrix_shape that calculates the shape
+of a matrix
+"""
+
+
 def matrix_shape(matrix):
     if not isinstance(matrix, list):
         return []
