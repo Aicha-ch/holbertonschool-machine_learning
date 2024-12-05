aef66597fdf077759e419999b943e85e55d4f923
Aicha-ch <chwikhi.aycha@gmail.com>
2024-12-05 17:26:24 +0100

    add documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -7,6 +7,17 @@ of a matrix
 
 
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix (list of lists).
+
+    Args:
+        matrix (list): A 2D list (matrix) whose shape we want to calculate.
+
+    Returns:
+        list: A list representing the shape of the matrix (its dimensions).
+              Each entry in the list represents the size of each dimension.
+              For example, for a 3x2 matrix, it returns [3, 2].
+    """
     if not isinstance(matrix, list):
         return []
     shape = []
