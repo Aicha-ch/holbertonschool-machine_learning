0a9ae5163883c0326527b2e99765afc0e5dbe1d8
Aicha-ch <chwikhi.aycha@gmail.com>
2024-12-06 09:28:43 +0100

    add documentation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 """
-A function `matrix_transpose` that calculates
-the transpose of a 2D matrix.
+A function that transposes a 2D matrix
 """
+
 
 def matrix_transpose(matrix):
     """
-    Transposes a matrix (swapping rows and columns).
-
-    Args:
-        matrix (list of list): A 2D list (matrix) where each inner list represents a row.
-
-    Returns:
-        list of list: A new 2D list representing the transposed matrix. 
-                      The rows of the original matrix become columns in the transposed matrix.
+    transposes a 2D matrix
     """
     return [list(row) for row in zip(*matrix)]
