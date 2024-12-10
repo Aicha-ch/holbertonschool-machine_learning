76011cdd1ff23bfd909a10169fbea871f533c4c5
Aicha-ch <chwikhi.aycha@gmail.com>
2024-12-10 18:00:15 +0100

    add documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Function that generates a plot and
+    displays it as a red solid line
+    """
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
