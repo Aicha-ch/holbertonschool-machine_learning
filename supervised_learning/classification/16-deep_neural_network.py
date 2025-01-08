125101a5ab21f46b5ba855952e0b28c2c6c269e6
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-08 22:06:09 +0100

    task16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
+"""
+This script defines a Deep Neural Network for binary classification.
+"""
+
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network"""
-
+    """
+    Define a deep neural network.
+    """
     def __init__(self, nx, layers):
-        """Initialize the deep neural network."""
+        """
+        Initialize a deep neural network.
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(nodes, int) and nodes > 0 for nodes in layers):
+
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for l in range(1, self.L + 1):
-            if l == 1:
-                self.weights[f'W{l}'] = (np.random.randn(layers[l - 1], nx) *
-                                         np.sqrt(2 / nx))
-            else:
-                self.weights[f'W{l}'] = (np.random.randn(layers[l - 1], layers[l - 2]) *
-                                         np.sqrt(2 / layers[l - 2]))
-            self.weights[f'b{l}'] = np.zeros((layers[l - 1], 1))
+        for layer_index in range(1, self.L + 1):
+            layer_size = layers[layer_index - 1]
+            prev_layer_size = nx if layer_index == 1 else layers[
+                    layer_index - 2
+                    ]
+            self.weights[f'W{layer_index}'] = (
+                    np.random.randn(layer_size, prev_layer_size) * np.sqrt(
+                        2 / prev_layer_size
+                        )
+                    )
+            self.weights[f'b{layer_index}'] = np.zeros((layer_size, 1))
