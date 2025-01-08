1ea8a07de6825b46d85a579b01a8d535f2db0369
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-08 03:56:13 +0100

    task 9

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -23,34 +23,57 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        # Output neuron parameters
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)  # Use internal attribute __W1
+        self.__b1 = np.zeros((nodes, 1))        # Use internal attribute __b1
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
         return self.__W1
 
+    @W1.setter
+    def W1(self, value):
+        self.__W1 = value
+
     @property
     def b1(self):
         return self.__b1
+
+    @b1.setter
+    def b1(self, value):
+        self.__b1 = value
 
     @property
     def A1(self):
         return self.__A1
 
+    @A1.setter
+    def A1(self, value):
+        self.__A1 = value
+
     @property
     def W2(self):
         return self.__W2
+
+    @W2.setter
+    def W2(self, value):
+        self.__W2 = value
 
     @property
     def b2(self):
         return self.__b2
 
+    @b2.setter
+    def b2(self, value):
+        self.__b2 = value
+
     @property
     def A2(self):
         return self.__A2
+
+    @A2.setter
+    def A2(self, value):
+        self.__A2 = value
