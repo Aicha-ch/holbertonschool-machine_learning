82d87fd4f15b956838c01487400f86720d4d7c90
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-08 22:11:21 +0100

    task 17

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+This script defines a Deep Neural Network for binary classification.
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Define a deep neural network.
+    """
+    def __init__(self, nx, layers):
+        """
+        Initialize a deep neural network.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or not layers:
+            raise TypeError("layers must be a list of positive integers")
+
+        if not all(map(lambda x: isinstance(x, int) and x > 0, layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for layer_index in range(1, self.L + 1):
+            layer_size = layers[layer_index - 1]
+            prev_layer_size = nx if layer_index == 1 else layers[
+                    layer_index - 2
+                    ]
+            self.weights[f'W{layer_index}'] = (
+                    np.random.randn(layer_size, prev_layer_size) * np.sqrt(
+                        2 / prev_layer_size
+                        )
+                    )
+            self.weights[f'b{layer_index}'] = np.zeros((layer_size, 1))
+    
+    @property
+    def L(self):
+        """
+        Getter for the number of layers in the neural network.
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        Getter for the cache dictionary.
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        Getter for the weights dictionary.
+        """
+        return self.__weights
+        
