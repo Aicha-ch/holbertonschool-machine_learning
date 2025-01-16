387340843189c7d836102fe61d5e4ae1879aa9c3
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-16 20:23:27 +0100

    placeholders for neural network

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Create placeholders
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """
+    Creating two placeholders, x and y, for the neural network.
+    """
+    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    return x, y
