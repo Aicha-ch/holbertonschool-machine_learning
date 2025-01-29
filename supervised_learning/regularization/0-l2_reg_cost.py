4a46aa9b549a73b1c8834656967ce074ccb0866e
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-29 18:07:02 +0100

    calculate the cost of neural network

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-"""
-Calculate the cost of a neural network.
-"""
-import numpy as np
+
+"""Calculate the cost of a neural network"""
 
 
 import numpy as np
+
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
-    calculate the cost of a neural network
+    Calculate the cost of a neural network
     """
-    sum_of_squares = 0
+    l2_cost = 0
+    for layer_idx in range(L):
+        l2_cost += np.sum(weights.get("W{}".format(layer_idx + 1))**2)
 
-    for l in range(1, L + 1):
-        sum_of_squares += np.sum(np.square(weights[f'W{l}']))
-
-    reg_cost = (lambtha / (2 * m)) * sum_of_squares
-    total_cost = cost + reg_cost
-
-    return total_cost
+    return cost + (lambtha / (2 * m)) * l2_cost
