d64d68cedb7c6e4488b4d6fbb33d94567fc7dae9
Aicha-ch <chwikhi.aycha@gmail.com>
2025-01-30 17:10:16 +0100

    TensorFlow layer with L2 regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Create a neural network layer with L2 regularization.
+Create a Layer with L2 Regularization.
 """
 
 import tensorflow as tf
@@ -8,12 +8,18 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creating a TensorFlow layer with L2 regularization.
+    Create a Layer with L2 Regularization.
     """
-    regularizer = tf.keras.regularizers.L2(lambtha)
+    init_weights = tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode="fan_avg"
+            )
+    l2_regularizer = tf.keras.regularizers.L2(lambtha)
+
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=regularizer
+        kernel_initializer=init_weights,
+        kernel_regularizer=l2_regularizer
     )
+
     return layer(prev)
