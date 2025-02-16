27c7d30a826af9e1a98af45b0197f8f9866af20c
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-16 15:19:02 +0100

    update fuction

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
+"""
+Building an identity block
+"""
 
-"""Identity Block"""
-
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
     """
-    Identity Block
+    Building an identity block
     """
-    init = K.initializers.he_normal()
-    f11, f3, f12 = filters
+    F11, F3, F12 = filters
 
-    conv_f11 = K.layers.Conv2D(filters=f11,
-                               kernel_size=(1, 1),
-                               padding="same",
-                               kernel_initializer=init)(A_prev)
-    norm_1 = K.layers.BatchNormalization(axis=3)(conv_f11)
-    act_1 = K.layers.ReLU()(norm_1)
+    init = K.initializers.HeNormal(seed=0)
 
-    conv_f3 = K.layers.Conv2D(filters=f3,
-                              kernel_size=(3, 3),
-                              padding="same",
-                              kernel_initializer=init)(act_1)
-    norm_2 = K.layers.BatchNormalization(axis=3)(conv_f3)
-    act_2 = K.layers.ReLU()(norm_2)
+    conv1 = K.layers.Conv2D(filters=F11,
+                            kernel_size=(1, 1),
+                            strides=(1, 1),
+                            padding="same",
+                            kernel_initializer=init)(A_prev)
 
-    conv_f12 = K.layers.Conv2D(filters=f12,
-                               kernel_size=(1, 1),
-                               padding="same",
-                               kernel_initializer=init)(act_2)
-    norm_3 = K.layers.BatchNormalization(axis=3)(conv_f12)
+    norm1 = K.layers.BatchNormalization(axis=-1)(conv1)
+    relu1 = K.layers.Activation(activation="relu")(norm1)
 
-    add = K.layers.Add()([norm_3, A_prev])
+    conv2 = K.layers.Conv2D(filters=F3,
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            padding="same",
+                            kernel_initializer=init)(relu1)
+    norm2 = K.layers.BatchNormalization(axis=-1)(conv2)
+    relu2 = K.layers.Activation(activation="relu")(norm2)
 
-    return K.layers.ReLU()(add)
+    conv3 = K.layers.Conv2D(filters=F12,
+                            kernel_size=(1, 1),
+                            strides=(1, 1),
+                            padding="same",
+                            kernel_initializer=init)(relu2)
+    norm3 = K.layers.BatchNormalization(axis=-1)(conv3)
+
+    merged = K.layers.Add()([norm3, A_prev])
+
+    return K.layers.Activation(activation="relu")(merged)
