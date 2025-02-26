8c294bbb838164f48ff8321e3f8c3d177170d74e
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-27 00:02:04 +0100

    create yolo class

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Initialize Yolo
+"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """Yolo v3 algorithm for object detection"""
+    
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Class constructor"""
+        self.model = K.models.load_model(model_path)
+        
+        with open(classes_path, 'r') as file:
+            self.class_names = [line.strip() for line in file]
+
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
