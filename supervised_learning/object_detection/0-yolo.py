0a0b1cb88f264f5a4dc829dd0c8044ea898d2dbd
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-27 00:03:50 +0100

    fixing style error

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -9,11 +9,11 @@ import numpy as np
 
 class Yolo:
     """Yolo v3 algorithm for object detection"""
-    
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Class constructor"""
         self.model = K.models.load_model(model_path)
-        
+
         with open(classes_path, 'r') as file:
             self.class_names = [line.strip() for line in file]
 
