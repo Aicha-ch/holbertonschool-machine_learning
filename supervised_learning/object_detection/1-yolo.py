891e4dc80dc0ac83480d91b216c12911d41bf127
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-28 00:39:49 +0100

    update code

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -30,31 +30,36 @@ class Yolo:
     Process the model outputs to extract bounding boxes
     confidences, and class probabilities.
     """
-    boxes, box_confidences, box_class_probs = [], [], []
-
-    input_size = np.array(self.model.inputs[0].shape.as_list()[1:3])
-    image_size = np.flip(image_size)
+    boxes = []
+    box_confidences = []
+    box_class_probs = []
 
     for i, output in enumerate(outputs):
         anchors = self.anchors[i]
         grid_h, grid_w = output.shape[:2]
 
-        t_xy, t_wh = output[..., :2], output[..., 2:4]
-        box_conf = np.expand_dims(self.sigmoid(output[..., 4]), axis=-1)
-        box_class_prob = self.sigmoid(output[..., 5:])
+        t_xy = output[..., :2]
+        t_wh = output[..., 2:4]
 
-        b_wh = (anchors * np.exp(t_wh)) / input_size
-        grid = np.indices((grid_w, grid_h)).transpose(1, 2, 0)
-        grid = np.tile(grid[..., np.newaxis, :], (1, 1, anchors.shape[0], 1))
+        sigmoid_conf = self.sigmoid(output[..., 4])
+        sigmoid_prob = self.sigmoid(output[..., 5:])
 
-        b_xy = (self.sigmoid(t_xy) + grid) / [grid_w, grid_h]
-        b_xy1, b_xy2 = b_xy - (b_wh / 2), b_xy + (b_wh / 2)
-        
+        box_conf = np.expand_dims(sigmoid_conf, axis=-1)
+        box_class_prob = sigmoid_prob
 
-        box = np.concatenate((b_xy1, b_xy2), axis=-1) * np.tile(image_size, 2)
+        b_wh = anchors * np.exp(t_wh)
+        b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+
+        grid = np.tile(np.indices((grid_width, grid_height)).T,
+                        anchors.shape[0]).reshape(
+                            (grid_height, grid_width) + anchors.shape)
+
+        b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
+
+        b_xy1 = b_xy - (b_wh / 2)
+        b_xy2 = b_xy + (b_wh / 2)
+        box = np.concatenate((b_xy1, b_xy2), axis=-1)
+        box *= np.tile(np.flip(image_size, axis=0), 2)
 
         boxes.append(box)
-        box_confidences.append(box_conf)
-        box_class_probs.append(box_class_prob)
-
-    return boxes, box_confidences, box_class_probs
+        return (boxes, box_confidences, box_class_probs)
