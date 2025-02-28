067b591e441700a70187f05089848408af9f8b5c
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-28 09:27:25 +0100

    update code

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -22,44 +22,48 @@ class Yolo:
         self.anchors = anchors
 
     def sigmoid(self, x):
-    """Sigmoid function"""
+        """Sigmoid function"""
         return 1 / (1 + np.exp(-x))
 
     def process_outputs(self, outputs, image_size):
-    """
-    Process the model outputs to extract bounding boxes
-    confidences, and class probabilities.
-    """
-    boxes = []
-    box_confidences = []
-    box_class_probs = []
+        """
+        Process the model outputs to extract bounding boxes,
+        confidences, and class probabilities.
+        """
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
 
-    for i, output in enumerate(outputs):
-        anchors = self.anchors[i]
-        grid_h, grid_w = output.shape[:2]
+        image_height, image_width = image_size
 
-        t_xy = output[..., :2]
-        t_wh = output[..., 2:4]
+        for i, output in enumerate(outputs):
+            grid_height, grid_width, anchor_boxes, _ = output.shape
+            anchors = self.anchors[i]
 
-        sigmoid_conf = self.sigmoid(output[..., 4])
-        sigmoid_prob = self.sigmoid(output[..., 5:])
+            t_xy = output[..., :2]
+            t_wh = output[..., 2:4]
 
-        box_conf = np.expand_dims(sigmoid_conf, axis=-1)
-        box_class_prob = sigmoid_prob
+            sigmoid_conf = self.sigmoid(output[..., 4])
+            sigmoid_prob = self.sigmoid(output[..., 5:])
 
-        b_wh = anchors * np.exp(t_wh)
-        b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+            box_conf = np.expand_dims(sigmoid_conf, axis=-1)
+            box_class_prob = sigmoid_prob
 
-        grid = np.tile(np.indices((grid_width, grid_height)).T,
-                        anchors.shape[0]).reshape(
-                            (grid_height, grid_width) + anchors.shape)
+            b_wh = anchors * np.exp(t_wh)
+            b_wh /= self.model.input.shape[1:3]
 
-        b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
+            grid = np.indices((grid_height, grid_width)).transpose(1, 2, 0)
+            grid = np.expand_dims(grid, axis=2)
 
-        b_xy1 = b_xy - (b_wh / 2)
-        b_xy2 = b_xy + (b_wh / 2)
-        box = np.concatenate((b_xy1, b_xy2), axis=-1)
-        box *= np.tile(np.flip(image_size, axis=0), 2)
+            b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
 
-        boxes.append(box)
-        return (boxes, box_confidences, box_class_probs)
+            b_xy1 = b_xy - (b_wh / 2)
+            b_xy2 = b_xy + (b_wh / 2)
+            box = np.concatenate((b_xy1, b_xy2), axis=-1)
+            box *= np.array([image_width, image_height, image_width, image_height])
+
+            boxes.append(box)
+            box_confidences.append(box_conf)
+            box_class_probs.append(box_class_prob)
+
+        return boxes, box_confidences, box_class_probs
