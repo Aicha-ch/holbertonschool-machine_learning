9cfc3550c37d0bd9a1e27c5fce49a1cde23dacfc
Aicha-ch <chwikhi.aycha@gmail.com>
2025-02-28 12:21:15 +0100

    add function that loads images

---
+++ supervised_learning/object_detection/4-yolo.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""
+Initialize Yolo
+"""
+
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """Yolo v3 algorithm for object detection"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Class constructor"""
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path, 'r') as file:
+            self.class_names = [line.strip() for line in file]
+
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """Sigmoid function"""
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """
+        Process the model outputs to extract bounding boxes,
+        confidences, and class probabilities.
+        """
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        image_height, image_width = image_size
+
+        for i, output in enumerate(outputs):
+            grid_height, grid_width = output.shape[:2]
+
+            t_x = output[..., 0]
+            t_y = output[..., 1]
+            t_w = output[..., 2]
+            t_h = output[..., 3]
+
+            c_x, c_y = np.meshgrid(np.arange(grid_width),
+                                   np.arange(grid_height))
+
+            c_x = np.expand_dims(c_x, axis=-1)
+            c_y = np.expand_dims(c_y, axis=-1)
+
+            bx = (self.sigmoid(t_x) + c_x) / grid_width
+            by = (self.sigmoid(t_y) + c_y) / grid_height
+            bw = (np.exp(t_w) * self.anchors[i,
+                  :, 0]) / self.model.input.shape[1]
+            bh = (np.exp(t_h) * self.anchors[i,
+                  :, 1]) / self.model.input.shape[2]
+
+            x1 = (bx - bw / 2) * image_width
+            y1 = (by - bh / 2) * image_height
+            x2 = (bx + bw / 2) * image_width
+            y2 = (by + bh / 2) * image_height
+
+            boxes.append(np.stack([x1, y1, x2, y2], axis=-1))
+            box_confidences.append(self.sigmoid(output[..., 4:5]))
+            box_class_probs.append(self.sigmoid(output[..., 5:]))
+
+        return boxes, box_confidences, box_class_probs
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """
+        Filters the bounding boxes.
+        """
+        filtered_boxes = []
+        box_classes = []
+        box_scores = []
+
+        for i in range(len(boxes)):
+            scores = box_confidences[i] * box_class_probs[i]
+            box_classes_i = np.argmax(scores, axis=-1)
+            box_scores_i = np.max(scores, axis=-1)
+            mask = box_scores_i >= self.class_t
+
+            filtered_boxes.append(boxes[i][mask])
+            box_classes.append(box_classes_i[mask])
+            box_scores.append(box_scores_i[mask])
+
+        filtered_boxes = np.concatenate(filtered_boxes, axis=0)
+        box_classes = np.concatenate(box_classes, axis=0)
+        box_scores = np.concatenate(box_scores, axis=0)
+
+        return filtered_boxes, box_classes, box_scores
+
+    def _iou(self, box1, box2):
+        """
+        Intersection Over Union of two bounding boxes
+        """
+        x1 = np.maximum(box1[0], box2[:, 0])
+        y1 = np.maximum(box1[1], box2[:, 1])
+        x2 = np.minimum(box1[2], box2[:, 2])
+        y2 = np.minimum(box1[3], box2[:, 3])
+
+        inter_area = np.maximum((x2 - x1), 0) * np.maximum((y2 - y1), 0)
+        box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+        box2_area = (box2[:, 2] - box2[:, 0]) * (box2[:, 3] - box2[:, 1])
+        union_area = box1_area + box2_area - inter_area
+
+        return inter_area / union_area
+
+    def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
+        """
+        Applying non-max suppression
+        """
+        indices = np.lexsort((-box_scores, box_classes))
+        filtered_boxes = filtered_boxes[indices]
+        box_scores = box_scores[indices]
+        box_classes = box_classes[indices]
+
+        unique_classes = np.unique(box_classes)
+        nms_boxes = []
+        nms_classes = []
+        nms_scores = []
+
+        for cls in unique_classes:
+            cls_indices = np.where(box_classes == cls)[0]
+            cls_boxes = filtered_boxes[cls_indices]
+            cls_scores = box_scores[cls_indices]
+
+            while len(cls_boxes) > 0:
+                max_score_index = np.argmax(cls_scores)
+                nms_boxes.append(cls_boxes[max_score_index])
+                nms_classes.append(cls)
+                nms_scores.append(cls_scores[max_score_index])
+
+                if len(cls_boxes) == 1:
+                    break
+
+                cls_boxes = np.delete(cls_boxes, max_score_index, axis=0)
+                cls_scores = np.delete(cls_scores, max_score_index)
+
+                ious = self._iou(nms_boxes[-1], cls_boxes)
+                iou_indices = np.where(ious <= self.nms_t)[0]
+
+                cls_boxes = cls_boxes[iou_indices]
+                cls_scores = cls_scores[iou_indices]
+
+        return (np.array(nms_boxes),
+                np.array(nms_classes),
+                np.array(nms_scores))
+
+    def load_images(folder_path):
+        """
+        Load images
+        """
+        image_paths = glob.glob(folder_path + "/*")
+        images = []
+        for image in image_paths:
+            images.append(cv2.imread(image))
+        return (images, image_paths)
