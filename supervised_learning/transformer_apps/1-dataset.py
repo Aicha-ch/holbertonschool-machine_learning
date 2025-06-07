7ed33c398b44781aaca637a16174f6182bfc418f
Aicha-ch <chwikhi.aycha@gmail.com>
2025-06-07 14:33:57 +0200

    loads and preps a dataset for machine translation

---
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Tokenize tensorflow Dataset
+"""
+
+import tensorflow_datasets as tfds
+import transformers
+
+
+class Dataset:
+    """
+    Prepare the TED HRLR translation dataset
+    for machine translation from Portuguese to English.
+    """
+
+    def __init__(self):
+        """
+        Initializing the Dataset object and loads the training and validation
+        datasets.
+        Initializing tokenizers for Portuguese and English.
+        """
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
+
+    def tokenize_dataset(self, data):
+        """
+        Tokenizing the dataset using pre-trained tokenizers and adapts them to
+        the dataset.
+        """
+        pt_sentences = []
+        en_sentences = []
+        for pt, en in data.as_numpy_iterator():
+            pt_sentences.append(pt.decode('utf-8'))
+            en_sentences.append(en.decode('utf-8'))
+
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased', use_fast=True,
+            clean_up_tokenization_spaces=True)
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            'bert-base-uncased', use_fast=True,
+            clean_up_tokenization_spaces=True)
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(pt_sentences,
+                                                            vocab_size=2**13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(en_sentences,
+                                                            vocab_size=2**13)
+
+        self.tokenizer_pt = tokenizer_pt
+        self.tokenizer_en = tokenizer_en
+
+        return self.tokenizer_pt, self.tokenizer_en
