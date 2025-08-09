d32ec08625539ea304ddbab79c89c47fe373e5fd
Aicha-ch <chwikhi.aycha@gmail.com>
2025-08-09 15:59:25 +0200

    Concatenate two pandas DataFrames

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Concatenate two pandas DataFrames.
+"""
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Concatenate two pandas DataFrames.
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2_filtered = df2[df2.index <= 1417411920]
+
+    concat = pd.concat([df2_filtered, df1], keys=["bitstamp", "coinbase"]
+                             )
+
+    return concat
